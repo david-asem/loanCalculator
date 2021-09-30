b830b04a2caef040d81bc873fc86f2576dc440e8
david-asem <ddy.asem@gmail.com>
2021-09-30 02:09:37 +0000

    add javascript funtions to calculate

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+//calculate results function
+function calculateResults(e) {
+  console.log('calculating....');
+
+  e.preventDefault();
+}
